251fee5c3b7683c98c134c8b67b16b9816730caa
hısaeeerq@gmail.com <yusufkmen@hotmail.com>
2025-03-22 22:30:25 +0300

    csv file installed

--- practise.py
+++ practise.py
@@ -1,10 +1,12 @@
 
-numInput = int(input("Please enter the base number: "))
-sqrNum = (lambda x: x ** 2)(numInput)
-print(sqrNum)
+with open("example.txt", "a") as file:
+    file.write("Yusuf\n")
+    file.write("Ökmen\n")
 
-num1 = int(input("Please enter the first number: "))
-num2 = int(input("Please enter the second number: "))
-sumNum = (lambda x, y: x + y)(num1, num2)
-print(sumNum)
+
+with open("example.txt", "r") as file:
+
+    content = file.read()
+
+    print(content)
 
