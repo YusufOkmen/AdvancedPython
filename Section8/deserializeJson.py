14ab1abec9666680ff8295233204a59c1182d671
hısaeeerq@gmail.com <yusufkmen@hotmail.com>
2025-03-28 17:22:33 +0300

    deserialize learned for json

---
+++ Section8/deserializeJson.py
@@ -0,0 +1,25 @@
+import json
+
+# with open(r"C:\Users\yusuf\OneDrive\Masaüstü\Coding\AdvancedPython\Section8\product.json")as file:
+#     data = json.loads(file)
+
+info = """
+{
+    "id": 1,
+    "title": "Excalibur G879",
+    "price": 100000,
+    "rating": 4.9,
+    "category": "Computer",
+    "colors": ["Black", "Gray"]
+}  
+"""
+
+data = json.loads(info)
+
+# print(data)
+print(data["title"])
+print(data["id"])
+print(data["colors"])
+
+# serialize = encode
+# deserialize = decode
