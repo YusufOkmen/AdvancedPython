e07ce00c821ac1adf3b11b7f592e4e6d732ce555
hısaeeerq@gmail.com <yusufkmen@hotmail.com>
2025-04-06 16:52:22 +0300

    learned how to scrap a website's data and storing the data in a CSV file

---
+++ Section10/quoteDemo.py
@@ -0,0 +1,30 @@
+import requests
+from bs4 import BeautifulSoup
+from csv import writer
+
+url = "https://quotes.toscrape.com/"
+
+response = requests.get(url)
+
+html = BeautifulSoup(response.text, "html.parser")
+
+quotes = html.find_all(class_="col-md-8")[1].find_all(class_="quote")
+
+with open("quotes.csv", "w", encoding="utf-8") as file:
+    csvWriter = writer(file)
+    csvWriter.writerow(["authors", "quotes", "tags"])
+
+
+
+    for quote in quotes:
+        authors = quote.find(class_="author").string
+        aList = []
+        tags = quote.find(class_="tags").find_all(class_="tag")
+        for a in tags:
+            newA = a.string
+            aList.append(newA)         
+        sentence = quote.find(class_="text").string
+        
+        csvWriter.writerow([authors, sentence, aList])
+
+print(aList)
